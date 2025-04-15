31e3e9da1b6269077075c226f5a6f6157049a850
highscopeDER <highscope@mail.ru>
2025-04-16 02:30:29 +0300

    trying to use docker

--- src/main/kotlin/com/example/plugins/Test.kt
+++ src/main/kotlin/com/example/plugins/Test.kt
@@ -1,22 +1,33 @@
 package com.example.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import org.jetbrains.exposed.sql.Database
 
-fun Application.connection() {
-//    Database.connect(
-//        url = "jdbc:postgresql://localhost:5432/project",
-//        user = "postgres",
-//        driver = "org.postgresql.Driver",
-//        password = "admin"
-//    )
+fun Application.connection(config: ApplicationConfig) {
+
+    val url = config.property("storage.jdbcURL").getString()
+    val user = config.property("storage.user").getString()
+    val password = config.property("storage.password").getString()
 
     Database.connect(
-        url = "jdbc:postgresql://localhost:5432/MIITNAV",
-        user = "postgres",
-        driver = "org.postgresql.Driver",
-        password = "admin"
+        url = url,
+        user = user,
+        password = password
     )
+
+}
+
+object Connection {
+
+    private const val BASE_URL: String = "jdbc:postgresql://localhost:5432/"
+
+    const val USER: String = "postgres"
+    const val DRIVER: String = "org.postgresql.Driver"
+    const val PASSWORD: String = "admin"
+
+    const val URL_V1: String = BASE_URL + "MIITNAV"
+    const val URL_V2: String = BASE_URL + "MIITNAV_V2"
 
 }
 
